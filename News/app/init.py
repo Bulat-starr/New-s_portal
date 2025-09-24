1c46635fa59c4457f3340126232386f54d5cacce
Aleksandr Kliuev <mkntsh.ltk@gmail.com>
2025-09-24 14:18:41 +0300

    Добавил документацию функций и классов

--- News/app/init.py
+++ News/app/init.py
@@ -2,6 +2,21 @@ from flask import Flask, render_template
 from config import config
 
 def createApp():
+    """
+        Фабричная функция для создания и настройки Flask приложения.
+
+        Этот подход позволяет создавать несколько экземпляров приложения
+        с разными конфигурациями (например, для тестирования).
+
+        Returns:
+            Flask: Настроенный экземпляр Flask приложения
+
+        Steps:
+            1. Создает экземпляр Flask приложения
+            2. Загружает конфигурацию из класса config
+            3. Регистрирует blueprint'ы (модули маршрутов)
+            4. Возвращает готовое приложение
+        """
     app = Flask(__name__)
     app.config.from_object(config)
     from routes import bp
--- News/app/routes.py
+++ News/app/routes.py
@@ -4,4 +4,19 @@ bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def index():
+    """
+        Обработчик главной страницы приложения.
+
+        Returns:
+            Response: HTML страница, отрендеренная из шаблона main_page.html
+
+        Functionality:
+            - Отображает главную страницу новостного агрегатора
+            - В будущем здесь будет передаваться список новостей в шаблон
+            - Подготавливается для добавления пагинации и фильтров
+
+        Template: main_page.html
+        Methods: GET only
+        """
+
     return render_template('main_page.html')
