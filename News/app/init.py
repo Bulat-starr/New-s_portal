933853690160474fad5bbc8b23811eb63e29f042
Se1dX <mkntsh.ltk@gmail.com>
2025-09-26 21:14:27 +0300

    В предыдущем коммите добавил бауз данных

---
+++ News/app/init.py
@@ -0,0 +1,35 @@
+import sqlalchemy
+from flask import Flask, render_template
+from config import config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def createApp():
+    """
+        Фабричная функция для создания и настройки Flask приложения.
+
+        Этот подход позволяет создавать несколько экземпляров приложения
+        с разными конфигурациями (например, для тестирования).
+
+        Returns:
+            Flask: Настроенный экземпляр Flask приложения
+
+        Steps:
+            1. Создает экземпляр Flask приложения
+            2. Загружает конфигурацию из класса config
+            3. Регистрирует blueprint'ы (модули маршрутов)
+            4. Возвращает готовое приложение
+        """
+    app = Flask(__name__)
+    app.config.from_object(config)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+    db.init_app(app)
+
+    from routes import bp
+    app.register_blueprint(bp)
+
+    return app
+
+
