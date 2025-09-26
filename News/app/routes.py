20e0fcbee2193b2e0bb677c09dabc3f86441ae3b
notforhealth <karasevnikolay1995@gmail.com>
2025-09-26 20:21:01 +0300

    Start working with script.js for infinite scrolling. Changes on main_page.html

--- News/app/routes.py
+++ News/app/routes.py
@@ -1,6 +1,20 @@
-from flask import Blueprint, render_template, request, jsonify, flash, redirect, url_for
+import random
+
+from flask import Blueprint, render_template, request, jsonify, flash, redirect, url_for, make_response
 
 bp = Blueprint('main', __name__)
+
+heading = "Lorem ipsum dolor sit amet"
+
+content = """
+Loren ipsum dolor sit amet consectetur adipisicing elit.
+Repellat inventore assumenda laboriosam,
+obcaecati saepe pariatur atque est? Quam, molestias nisi.
+"""
+
+posts = 200
+quantity = 10
+db = list()
 
 @bp.route('/')
 def index():
@@ -20,3 +34,28 @@ def index():
         """
 
     return render_template('index.html')
+
+for x in range(posts):
+    headingParts = heading.split(" ")
+    random.shuffle(headingParts)
+
+    contentParts = content.split(" ")
+    random.shuffle(contentParts)
+
+    db.append([x, " ".join(headingParts), " ".join(contentParts)])
+
+
+@bp.route('/news')
+def load():
+    if request.args:
+        counter = int(request.args.get("c"))
+        if counter == 0:
+            print(f"Returning posts 0 to {quantity}")
+            res = make_response(jsonify(db[0:quantity]), 200)
+        elif counter == posts:
+            print("No more posts")
+            res = make_response(jsonify({}), 200)
+        else:
+            print(f"Returning posts {counter} to {counter + quantity}")
+            res = make_response(jsonify(db[counter:counter + quantity]), 200)
+    return res
